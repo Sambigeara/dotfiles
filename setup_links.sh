5ace3d108ade537b8a4e33dab5728a66eab791f0
Sam Lock <sam@swlock.co.uk>
2024-04-10 08:56:12 +0100

    add yabai and skhd
    Signed-off-by: Sam Lock <sam@swlock.co.uk>

--- setup_links.sh
+++ setup_links.sh
@@ -3,16 +3,22 @@
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # nvim
-ln -sfn $DOTFILES_DIR/nvim ~/.config/nvim
+ln -sfn "$DOTFILES_DIR/nvim" ~/.config/nvim
 
 # alacritty
-ln -sfn $DOTFILES_DIR/alacritty ~/.config/alacritty
+ln -sfn "$DOTFILES_DIR/alacritty" ~/.config/alacritty
 
 # fish
-ln -sfn $DOTFILES_DIR/fish ~/.config/fish
+ln -sfn "$DOTFILES_DIR/fish" ~/.config/fish
 
 # tmux
-ln -sf $DOTFILES_DIR/tmux/.tmux.conf ~/.tmux.conf
+ln -sf "$DOTFILES_DIR/tmux/.tmux.conf" ~/.tmux.conf
+
+# skhd
+ln -sfn "$DOTFILES_DIR/skhd" ~/.config/skhd
+
+# yabai
+ln -sfn "$DOTFILES_DIR/yabai" ~/.config/yabai
 
 git submodule update --init --recursive
 
