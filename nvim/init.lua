914ddbb1550dad4b1135a44363390d4c24758c08
Sam Lock <sam@swlock.co.uk>
2026-03-18 18:31:17 +0000

    bits
    Signed-off-by: Sam Lock <sam@swlock.co.uk>

--- nvim/init.lua
+++ nvim/init.lua
@@ -33,6 +33,7 @@ vim.o.softtabstop = 4
 vim.o.autoindent = true
 vim.o.wrap = true
 vim.o.breakindent = true
+vim.opt.fillchars = { eob = "~" }
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
@@ -274,6 +275,11 @@ require("lazy").setup({
 				skip_confirm_for_simple_edits = true,
 			})
 			vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+			vim.keymap.set("n", "<BS>", function()
+				if vim.bo.filetype == "oil" then
+					vim.cmd("bd")
+				end
+			end, { desc = "Close oil" })
 		end,
 	},
 
@@ -418,9 +424,74 @@ require("lazy").setup({
 
 	{
 		"ggandor/leap.nvim",
-		keys = { "s", "S" },
+		url = "https://codeberg.org/andyg/leap.nvim",
+		keys = {
+			{ "s", "<Plug>(leap)", mode = { "n", "x", "o" }, desc = "Leap forward" },
+			{ "S", "<Plug>(leap-from-window)", mode = { "n", "x", "o" }, desc = "Leap from window" },
+		},
+	},
+
+	{
+		"nickjvandyke/opencode.nvim",
+		version = "*", -- Latest stable release
+		dependencies = {
+			{
+				-- `snacks.nvim` integration is recommended, but optional
+				---@module "snacks" <- Loads `snacks.nvim` types for configuration intellisense
+				"folke/snacks.nvim",
+				optional = true,
+				opts = {
+					input = {}, -- Enhances `ask()`
+					picker = { -- Enhances `select()`
+						actions = {
+							opencode_send = function(...)
+								return require("opencode").snacks_picker_send(...)
+							end,
+						},
+						win = {
+							input = {
+								keys = {
+									["<a-a>"] = { "opencode_send", mode = { "n", "i" } },
+								},
+							},
+						},
+					},
+					terminal = {}, -- Enables the `snacks` provider
+				},
+			},
+		},
 		config = function()
-			require("leap").add_default_mappings()
+			---@type opencode.Opts
+			vim.g.opencode_opts = {
+				-- Your configuration, if any; goto definition on the type or field for details
+			}
+
+			vim.o.autoread = true -- Required for `opts.events.reload`
+
+			-- Recommended/example keymaps
+			vim.keymap.set({ "n", "x" }, "<leader>oa", function()
+				require("opencode").ask("@this: ", { submit = true })
+			end, { desc = "Ask opencode…" })
+			vim.keymap.set({ "n", "x" }, "<leader>os", function()
+				require("opencode").select()
+			end, { desc = "Execute opencode action…" })
+			vim.keymap.set({ "n", "t" }, "<leader>oo", function()
+				require("opencode").toggle()
+			end, { desc = "Toggle opencode" })
+
+			vim.keymap.set({ "n", "x" }, "go", function()
+				return require("opencode").operator("@this ")
+			end, { desc = "Add range to opencode", expr = true })
+			vim.keymap.set("n", "goo", function()
+				return require("opencode").operator("@this ") .. "_"
+			end, { desc = "Add line to opencode", expr = true })
+
+			vim.keymap.set("n", "<leader>ou", function()
+				require("opencode").command("session.half.page.up")
+			end, { desc = "Scroll opencode up" })
+			vim.keymap.set("n", "<leader>od", function()
+				require("opencode").command("session.half.page.down")
+			end, { desc = "Scroll opencode down" })
 		end,
 	},
 
@@ -595,7 +666,7 @@ require("lazy").setup({
 
 			require("mason-lspconfig").setup({
 				ensure_installed = {},
-				automatic_installation = false,
+				automatic_installation = true,
 				handlers = {
 					function(server_name)
 						local server = servers[server_name] or {}
